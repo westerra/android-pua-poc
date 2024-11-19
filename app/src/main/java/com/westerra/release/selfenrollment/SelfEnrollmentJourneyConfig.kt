a865d42434db6dabb982ddf8425ac65bcdd42cc9
Akbar <akbar@backbase.com>
2024-11-20 00:43:32 +0530

    PUA : Fixed SelfEnrollment Journey related issues. - Kindly review and compare the OOTB XML layouts with the Westerra app to incorporate the latest UI changes effectively.

--- app/src/main/java/com/westerra/release/selfenrollment/SelfEnrollmentJourneyConfig.kt
+++ app/src/main/java/com/westerra/release/selfenrollment/SelfEnrollmentJourneyConfig.kt
@@ -1,7 +1,7 @@
 package com.westerra.release.selfenrollment
 
-import com.backbase.android.identity.journey.self_enrollment.configuration.MainScreenConfiguration
 import com.backbase.android.identity.journey.self_enrollment.configuration.SelfEnrollmentJourneyConfiguration
+import com.backbase.android.identity.journey.self_enrollment.configuration.SelfEnrollmentScreenConfiguration
 import com.westerra.release.BuildConfig
 import com.westerra.release.R
 import com.westerra.release.constants.Constants
@@ -25,11 +25,19 @@ object SelfEnrollmentJourneyConfig {
 
             background = R.color.authenticationBackgroundColor.toDeferredDrawable()
             textColor = R.color.textColorPrimary.toDeferredColor()
-            mainScreenConfiguration = MainScreenConfiguration {
+
+            //TODO- 'mainScreenConfiguration: MainScreenConfiguration' is deprecated. MainScreenConfiguration is deprecated and will be removed in v4.0 at which time SelfEnrollmentScreenConfiguration will be the default screen configuration.
+            //TODO - Override string resource to set different text for deprecated views.
+//            mainScreenConfiguration = MainScreenConfiguration {
+//                icon = R.drawable.westerra_logo_stacked.toDeferredDrawable()
+//                welcomeMessageTitleText = R.string.welcome_message.toDeferredText()
+//                enrollButtonText = R.string.enroll_digital_banking.toDeferredText()
+//            }
+
+            selfEnrollmentScreenConfiguration = SelfEnrollmentScreenConfiguration.Builder().apply{
                 icon = R.drawable.westerra_logo_stacked.toDeferredDrawable()
-                welcomeMessageTitleText = R.string.welcome_message.toDeferredText()
-                enrollButtonText = R.string.enroll_digital_banking.toDeferredText()
-            }
+
+            }.build()
         }
     }
 }
