513b62d9ee1d09a1a89584a4d17217e98b55438d
Akbar <akbar@backbase.com>
2024-11-20 00:12:51 +0530

    PUA: Adopted new client and updated latest clients as in Retail 2024.03

--- app/src/main/java/com/westerra/release/WesterraClientDefinitions.kt
+++ app/src/main/java/com/westerra/release/WesterraClientDefinitions.kt
@@ -1,28 +1,38 @@
 package com.westerra.release
 
 import android.app.Application
-import com.backbase.android.client.accesscontrolclient2.api.UserContextApi
-import com.backbase.android.client.accesscontrolclient2.api.UsersApi
-import com.backbase.android.client.accountstatementsclient2.api.AccountStatementApi
-import com.backbase.android.client.actionclient2.api.ActionRecipesApi
-import com.backbase.android.client.cardsclient2.api.CardsApi
-import com.backbase.android.client.cardsclient2.api.TravelNoticesApi
 import com.backbase.android.client.contactmanagerclient2.api.ContactsApi
+import com.backbase.android.client.gen2.accesscontrolclient3.api.UserContextApi
+import com.backbase.android.client.gen2.accesscontrolclient3.api.UsersApi
+import com.backbase.android.client.gen2.accountstatementsclient2.api.AccountStatementApi
 import com.backbase.android.client.gen2.addressautocompleteclient1.api.AddressAutocompleteApi
+import com.backbase.android.client.gen2.arrangementclient2.api.ArrangementViewsApi
 import com.backbase.android.client.gen2.arrangementclient2.api.ArrangementsApi
 import com.backbase.android.client.gen2.arrangementclient2.api.ProductSummaryApi
+import com.backbase.android.client.gen2.beneficiaryvalidationclient1.api.BeneficiaryValidationApi
+import com.backbase.android.client.gen2.billpayintegratorclient2.api.BillPaySsoApi
+import com.backbase.android.client.gen2.budgetsclient3.api.BudgetsApi
+import com.backbase.android.client.gen2.cardsclient2.api.CardProductsApi
+import com.backbase.android.client.gen2.cardsclient2.api.CardsApi
+import com.backbase.android.client.gen2.cardsclient2.api.TravelNoticesApi
+import com.backbase.android.client.gen2.cardswalletclient1.api.WalletApi
+import com.backbase.android.client.gen2.engagementclient1.api.EngagementMetricsApi
 import com.backbase.android.client.gen2.engagementclient1.api.NotificationPreferenceApi
 import com.backbase.android.client.gen2.engagementclient1.api.SelectBannerApi
 import com.backbase.android.client.gen2.externalaccountaggregatorclient1.api.ExternalAccountAggregationClientApi
 import com.backbase.android.client.gen2.financialinstitutionmanagerclient1.api.FinancialInstitutionManagerClientApi
 import com.backbase.android.client.gen2.loansclient1.api.LoansApi
 import com.backbase.android.client.gen2.messageclient5.api.MessagecenterApi
+import com.backbase.android.client.gen2.metricclient1.api.InteractionApi
+import com.backbase.android.client.gen2.paymentordera2aclient1.api.A2aClientApi
 import com.backbase.android.client.gen2.paymentorderv2client2.api.PaymentOrdersApi as PaymentOrdersV2Api
 import com.backbase.android.client.gen2.placemanagerclient2.api.PlacesApi
 import com.backbase.android.client.gen2.remotedepositcapturerclient2.api.RemoteDepositCapturerClientApi
 import com.backbase.android.client.gen2.rtcclient1.api.AccessApi
 import com.backbase.android.client.gen2.rtcclient1.api.ConversationsApi
 import com.backbase.android.client.gen2.rtcclient1.api.QueuesApi
+import com.backbase.android.client.gen2.savvymoneycreditscorerclient1.api.SavvyMoneyCreditScorerApi
+import com.backbase.android.client.gen2.transactioncategorycollectorclient2.api.CategoriesApi
 import com.backbase.android.client.gen2.transactionsclient2.api.TransactionClientApi as TransactionV2ClientApi
 import com.backbase.android.client.notificationclient2.api.NotificationsApi
 import com.backbase.android.client.paymentorderclient2.api.PaymentOrdersApi
@@ -36,8 +46,11 @@ import com.backbase.android.identity.journey.userprofile.UserProfileJourneyScope
 import com.backbase.android.identity.journey.userprofile.address.autocomplete.UserProfileAddressSearchResultsProvider
 import com.backbase.android.retail.journey.accounts_and_transactions.AccountsAndTransactionsJourneyScope
 import com.backbase.android.retail.journey.accountstatements.AccountStatementsJourneyScope
+import com.backbase.android.retail.journey.billPaySso.BillPayIntegratorSsoJourneyScope
 import com.backbase.android.retail.journey.cardsmanagement.CardsManagementJourneyScope
 import com.backbase.android.retail.journey.contacts.ContactsJourneyScope
+import com.backbase.android.retail.journey.credit_score.CreditScoreJourneyScope
+import com.backbase.android.retail.journey.financialinsights.FinancialInsightsJourneyScope
 import com.backbase.android.retail.journey.payments.PaymentJourneyScope
 import com.backbase.android.retail.journey.payments.upcoming.UpcomingPaymentsJourneyScope
 import com.backbase.android.retail.journey.places.PlacesJourneyScope
@@ -47,11 +60,14 @@ import com.backbase.android.retail.journey.rdh.RemoteDepositHistoryJourneyScope
 import com.backbase.android.retail.journey.rtc.RTCJourneyScope
 import com.backbase.engagementchannels.messages.MessagesJourneyScope
 import com.backbase.engagementchannels.notifications.NotificationsJourneyScope
+import com.westerra.release.QuickActionQualifiers.NOTIFICATION_CLIENT_2
+import com.westerra.release.QuickActionQualifiers.NOTIFICATION_CLIENT_3
 import java.net.URI
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.ModuleDeclaration
-import org.koin.dsl.bind
 import org.koin.dsl.module
+import com.backbase.android.client.gen2.notificationclient3.api.NotificationsApi as NotificationsV3ClientApi
+
 
 /**
  * **Note**: Please arrange alphabetically by `scope` or `dependency` name, also maintain definition grouping as per
@@ -89,7 +105,7 @@ fun WesterraClientDefinitions(additionalDependencies: ModuleDeclaration = {}) = 
         )
     }
 
-    single {
+    single(qualifier = NOTIFICATION_CLIENT_2)  {
         val serverUri = URI("${apiRoot()}/notifications-service")
         NotificationsApi(
             context = get<Application>(),
@@ -98,6 +114,18 @@ fun WesterraClientDefinitions(additionalDependencies: ModuleDeclaration = {}) = 
             serverUri = serverUri,
             provider = get(),
             backbase = get(),
+        )
+    }
+
+    single(qualifier = NOTIFICATION_CLIENT_3) {
+        val serverUri = URI("${apiRoot()}/notifications-service")
+        NotificationsV3ClientApi(
+            context = get<Application>(),
+            moshi = get(),
+            parser = get(),
+            serverUri = serverUri,
+            provider = get(),
+            backbase = get()
         )
     }
 
@@ -114,6 +142,30 @@ fun WesterraClientDefinitions(additionalDependencies: ModuleDeclaration = {}) = 
                 serverUri = serverUri,
                 provider = get(),
                 backbase = get(),
+            )
+        }
+
+        scoped {
+            val serverUri = URI("${apiRoot()}/engagement")
+            EngagementMetricsApi(
+                context = get<Application>(),
+                moshi = get(),
+                parser = get(),
+                serverUri = serverUri,
+                provider = get(),
+                backbase = get()
+            )
+        }
+
+        scoped {
+            val serverUri = URI("${apiRoot()}/metric")
+            InteractionApi(
+                context = get<Application>(),
+                moshi = get(),
+                parser = get(),
+                serverUri = serverUri,
+                provider = get(),
+                backbase = get()
             )
         }
 
@@ -186,6 +238,17 @@ fun WesterraClientDefinitions(additionalDependencies: ModuleDeclaration = {}) = 
                 serverUri = uri,
             )
         }
+
+        scoped {
+            val uri = URI("${apiRoot()}/arrangement-manager")
+            ArrangementViewsApi(
+                context = androidContext(),
+                moshi = get(),
+                parser = get(),
+                backbase = get(),
+                serverUri = uri,
+            )
+        }
     }
 
     scope<AccountStatementsJourneyScope> {
@@ -196,6 +259,19 @@ fun WesterraClientDefinitions(additionalDependencies: ModuleDeclaration = {}) = 
                 moshi = get(),
                 parser = get(),
                 serverUri = serverUri,
+                provider = get(),
+                backbase = get(),
+            )
+        }
+    }
+
+    scope<BillPayIntegratorSsoJourneyScope> {
+        scoped {
+            BillPaySsoApi(
+                context = get<Application>(),
+                moshi = get(),
+                parser = get(),
+                serverUri = URI.create("${apiRoot()}/billpay-integrator"),
                 provider = get(),
                 backbase = get(),
             )
@@ -238,7 +314,82 @@ fun WesterraClientDefinitions(additionalDependencies: ModuleDeclaration = {}) = 
                 provider = NetworkDBSDataProvider(androidContext()),
             )
         }
+
+        scoped {
+            WalletApi(
+                context = get<Application>(),
+                moshi = get(),
+                parser = get(),
+                serverUri = URI("${apiRoot()}/card-manager"),
+                provider = get()
+            )
+        }
+
+        scoped {
+            val serverUri = URI("${apiRoot()}/cards-presentation-service")
+            CardProductsApi(
+                context = get<Application>(),
+                moshi = get(),
+                parser = get(),
+                serverUri = serverUri,
+                provider = get()
+            )
+        }
     }
+
+    scope<CreditScoreJourneyScope> {
+        scoped {
+            val serverUri = URI("${apiRoot()}/savvy-money-credit-scorer")
+            SavvyMoneyCreditScorerApi(
+                context = get<Application>(),
+                moshi = get(),
+                parser = get(),
+                serverUri = serverUri,
+                provider = get(),
+                backbase = get(),
+            )
+        }
+    }
+
+    scope<FinancialInsightsJourneyScope> {
+        scoped {
+            val serverUri = URI("${apiRoot()}/income-expense-analyzer")
+
+            com.backbase.android.client.gen2.incomeexpenseanalyzerclient1.api.CategoryTotalsApi(
+                context = get<Application>(),
+                moshi = get(),
+                parser = get(),
+                serverUri = serverUri,
+                provider = get(),
+                backbase = get()
+            )
+        }
+
+        scoped {
+            val serverUri = URI("${apiRoot()}/transaction-category-collector")
+            CategoriesApi(
+                context = get<Application>(),
+                moshi = get(),
+                parser = get(),
+                serverUri = serverUri,
+                provider = get(),
+                backbase = get()
+            )
+        }
+
+        scoped {
+            val serverUri = URI("${apiRoot()}/budget-planner")
+            BudgetsApi(
+                context = get<Application>(),
+                moshi = get(),
+                parser = get(),
+                serverUri = serverUri,
+                provider = get(),
+                backbase = get()
+            )
+        }
+    }
+
 
     scope<ContactsJourneyScope> {
         scoped {
@@ -281,18 +432,18 @@ fun WesterraClientDefinitions(additionalDependencies: ModuleDeclaration = {}) = 
             )
         }
 
-        scoped {
-            val serverUri = URI("${apiRoot()}/action")
-
-            ActionRecipesApi(
-                context = get<Application>(),
-                moshi = get(),
-                parser = get(),
-                serverUri = serverUri,
-                provider = get(),
-                backbase = get(),
-            )
-        }
+//        scoped {
+//            val serverUri = URI("${apiRoot()}/action")
+//
+//            ActionRecipesApi(
+//                context = get<Application>(),
+//                moshi = get(),
+//                parser = get(),
+//                serverUri = serverUri,
+//                provider = get(),
+//                backbase = get(),
+//            )
+//        }
 
         scoped {
             val serverUri = URI("${apiRoot()}/arrangement-manager")
@@ -321,17 +472,28 @@ fun WesterraClientDefinitions(additionalDependencies: ModuleDeclaration = {}) = 
 
     scope<PaymentJourneyScope> {
         scoped {
-            val networkDBSDataProvider = NetworkDBSDataProvider(get<Application>())
-            val serverUri = URI.create("${apiRoot()}/payment-order-service")
-            com.backbase.android.client.gen2.paymentorderclient2.api.PaymentOrdersApi(
+            val uri = URI("${apiRoot()}/payment-order-service")
+            PaymentOrdersV2Api(
                 context = get<Application>(),
                 moshi = get(),
                 parser = get(),
-                serverUri = serverUri,
-                provider = networkDBSDataProvider,
-                backbase = get(),
+                serverUri = uri,
+                backbase = get()
             )
         }
+
+//        scoped {
+//            val networkDBSDataProvider = NetworkDBSDataProvider(get<Application>())
+//            val serverUri = URI.create("${apiRoot()}/payment-order-service")
+//            com.backbase.android.client.gen2.paymentorderclient2.api.PaymentOrdersApi(
+//                context = get<Application>(),
+//                moshi = get(),
+//                parser = get(),
+//                serverUri = serverUri,
+//                provider = networkDBSDataProvider,
+//                backbase = get(),
+//            )
+//        }
 
         scoped {
             val networkDBSDataProvider = NetworkDBSDataProvider(get<Application>())
@@ -348,6 +510,30 @@ fun WesterraClientDefinitions(additionalDependencies: ModuleDeclaration = {}) = 
 
         scoped {
             val networkDBSDataProvider = NetworkDBSDataProvider(get<Application>())
+            val serverUri = URI.create("${apiRoot()}/payment-order-a2a")
+            A2aClientApi(
+                context = get<Application>(),
+                moshi = get(),
+                parser = get(),
+                serverUri = serverUri,
+                provider = networkDBSDataProvider,
+                backbase = get()
+            )
+        }
+
+        scoped {
+            com.backbase.android.client.gen2.contactmanagerclient2.api.ContactsApi(
+                context = get<Application>(),
+                moshi = get(),
+                parser = get(),
+                serverUri = URI.create("${apiRoot()}/contact-manager"),
+                provider = get(),
+                backbase = get(),
+            )
+        }
+
+        scoped {
+            val networkDBSDataProvider = NetworkDBSDataProvider(get<Application>())
             val serverUri = URI.create("${apiRoot()}/arrangement-manager")
             ArrangementsApi(
                 context = get<Application>(),
@@ -356,6 +542,18 @@ fun WesterraClientDefinitions(additionalDependencies: ModuleDeclaration = {}) = 
                 serverUri = serverUri,
                 provider = networkDBSDataProvider,
                 backbase = get(),
+            )
+        }
+
+        scoped {
+            val serverUri = URI("${apiRoot()}/beneficiary-validation-service")
+            BeneficiaryValidationApi(
+                context = get<Application>(),
+                moshi = get(),
+                parser = get(),
+                serverUri = serverUri,
+                provider = get(),
+                backbase = get()
             )
         }
     }
@@ -374,15 +572,14 @@ fun WesterraClientDefinitions(additionalDependencies: ModuleDeclaration = {}) = 
 
         scoped {
             val uri = URI("${apiRoot()}/payment-order-service")
-            PaymentOrdersApi(
+            PaymentOrdersV2Api(
                 context = get<Application>(),
                 moshi = get(),
                 parser = get(),
                 serverUri = uri,
-                backbase = get(),
+                backbase = get()
             )
         }
-
         scoped {
             val serverUri = URI("${apiRoot()}/arrangement-manager")
             ProductSummaryApi(
@@ -402,6 +599,18 @@ fun WesterraClientDefinitions(additionalDependencies: ModuleDeclaration = {}) = 
                 parser = get(),
                 serverUri = uri,
                 backbase = get(),
+            )
+        }
+
+        scoped {
+            val serverUri = URI("${apiRoot()}/arrangement-manager")
+            ArrangementsApi(
+                context = get<Application>(),
+                moshi = get(),
+                parser = get(),
+                backbase = get(),
+                provider = get(),
+                serverUri = serverUri,
             )
         }
     }
@@ -447,13 +656,13 @@ fun WesterraClientDefinitions(additionalDependencies: ModuleDeclaration = {}) = 
     }
 
     scope<UserProfileJourneyScope> {
-        scoped {
-            UserProfileAddressSearchResultsProvider(
-                get<UserProfileConfiguration>().addAddressScreen.addressAutocompleteConfiguration,
-                get(),
-                get<Application>(),
-            )
-        } bind AddressSearchResultsProvider::class
+//        scoped {
+//            UserProfileAddressSearchResultsProvider(
+//                get<UserProfileConfiguration>().addAddressScreen.addressAutocompleteConfiguration,
+//                get(),
+//                get<Application>(),
+//            )
+//        } bind AddressSearchResultsProvider::class
 
         scoped {
             val serverUri = URI("${apiRoot()}/user-manager")
@@ -556,6 +765,41 @@ fun WesterraClientDefinitions(additionalDependencies: ModuleDeclaration = {}) = 
                 backbase = get(),
             )
         }
+    }
+
+    single {
+        val serverUri = URI("${apiRoot()}/engagement")
+        SelectBannerApi(
+            context = get<Application>(),
+            moshi = get(),
+            parser = get(),
+            serverUri = serverUri,
+            provider = get(),
+            backbase = get()
+        )
+    }
+    single {
+        val serverUri = URI("${apiRoot()}/engagement")
+        EngagementMetricsApi(
+            context = get<Application>(),
+            moshi = get(),
+            parser = get(),
+            serverUri = serverUri,
+            provider = get(),
+            backbase = get()
+        )
+    }
+
+    single {
+        val serverUri = URI("${apiRoot()}/metric")
+        InteractionApi(
+            context = get<Application>(),
+            moshi = get(),
+            parser = get(),
+            serverUri = serverUri,
+            provider = get(),
+            backbase = get()
+        )
     }
     //endregion
 
