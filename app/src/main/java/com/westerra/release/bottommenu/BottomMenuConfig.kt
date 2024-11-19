aa3264686a7bf96f4189905a164e43431cc79772
Akbar <akbar@backbase.com>
2024-11-20 00:47:57 +0530

    PUA : Updated BottomMenu config by referring OOTB Retail US config.

--- app/src/main/java/com/westerra/release/bottommenu/BottomMenuConfig.kt
+++ app/src/main/java/com/westerra/release/bottommenu/BottomMenuConfig.kt
@@ -4,8 +4,12 @@ import androidx.core.os.bundleOf
 import com.backbase.android.retail.journey.app.common.menu.BottomMenuConfiguration
 import com.backbase.android.retail.journey.app.common.menu.BottomMenuItem
 import com.backbase.android.retail.journey.app.us.DefaultUsAccountsBottomMenuItem
+import com.backbase.android.retail.journey.app.us.DefaultUsDashboardBottomMenuItem
 import com.backbase.android.retail.journey.app.us.DefaultUsMoreBottomMenuItem
 import com.backbase.android.retail.journey.app.us.DefaultUsMoveMoneyBottomMenuItem
+import com.backbase.android.retail.journey.app.us.DefaultUsTabbedHeaderBottomMenuConfiguration
+import com.backbase.android.retail.journey.app.us.UsApplicationFeatureFlag
+import com.backbase.android.retail.journey.app.us.hasFeatureFlag
 import com.backbase.android.retail.journey.more.MoreJourney
 import com.backbase.android.retail.journey.more.MoreMenuInstanceId
 import com.westerra.release.R
@@ -17,31 +21,65 @@ import com.westerra.release.extensions.toDeferredText
 object BottomMenuConfig {
 
     operator fun invoke(): BottomMenuConfiguration {
-        return BottomMenuConfiguration {
-            items = listOf(
-                DefaultUsAccountsBottomMenuItem(),
-                DefaultUsMoveMoneyBottomMenuItem {
-                    navigationResId = R.navigation.westerra_app_more_tab
-                    startDestinationArgs =
-                        bundleOf(
-                            MoreJourney.INSTANCE_ID to
-                                MoreMenuInstanceId.Custom(name = MENU_INSTANCE_ID_MOVE_MONEY),
-                        )
-                },
-                BottomMenuItem {
-                    title = R.string.deposit_check_title.toDeferredText()
-                    icon = R.drawable.ic_rdc.toDeferredDrawable()
-                    navigationResId = R.navigation.westerra_app_more_tab
-                    startDestinationArgs =
-                        bundleOf(
-                            MoreJourney.INSTANCE_ID to
-                                MoreMenuInstanceId.Custom(name = MENU_INSTANCE_ID_DEPOSIT_A_CHECK),
-                        )
-                },
-                // Disable cards management until requirement comes
-                // DefaultUsCardsBottomMenuItem(),
-                DefaultUsMoreBottomMenuItem(),
-            )
+
+        return if (hasFeatureFlag<UsApplicationFeatureFlag.UseTabbedHeaderFeatureFlag>()) {
+            DefaultUsTabbedHeaderBottomMenuConfiguration()
+        } else if (hasFeatureFlag<UsApplicationFeatureFlag.DashboardJourneyFeatureFlag>()) {
+            BottomMenuConfiguration {
+                items = listOf(
+                    DefaultUsDashboardBottomMenuItem(),
+                    DefaultUsAccountsBottomMenuItem(),
+                    DefaultUsMoveMoneyBottomMenuItem {
+                        navigationResId = R.navigation.westerra_app_more_tab
+                        startDestinationArgs =
+                            bundleOf(
+                                MoreJourney.INSTANCE_ID to
+                                        MoreMenuInstanceId.Custom(name = MENU_INSTANCE_ID_MOVE_MONEY),
+                            )
+                    },
+                    BottomMenuItem {
+                        title = R.string.deposit_check_title.toDeferredText()
+                        icon = R.drawable.ic_rdc.toDeferredDrawable()
+                        navigationResId = R.navigation.westerra_app_more_tab
+                        startDestinationArgs =
+                            bundleOf(
+                                MoreJourney.INSTANCE_ID to
+                                        MoreMenuInstanceId.Custom(name = MENU_INSTANCE_ID_DEPOSIT_A_CHECK),
+                            )
+                    },
+                    // Disable cards management until requirement comes
+                    //DefaultUsCardsBottomMenuItem(),
+                    DefaultUsMoreBottomMenuItem(),
+                )
+            }
+        } else {
+            BottomMenuConfiguration {
+                items = listOf(
+                    DefaultUsAccountsBottomMenuItem(),
+                   // DefaultUsPocketsBottomMenuItem(),
+                    DefaultUsMoveMoneyBottomMenuItem {
+                        navigationResId = R.navigation.westerra_app_more_tab
+                        startDestinationArgs =
+                            bundleOf(
+                                MoreJourney.INSTANCE_ID to
+                                        MoreMenuInstanceId.Custom(name = MENU_INSTANCE_ID_MOVE_MONEY),
+                            )
+                    },
+                    BottomMenuItem {
+                        title = R.string.deposit_check_title.toDeferredText()
+                        icon = R.drawable.ic_rdc.toDeferredDrawable()
+                        navigationResId = R.navigation.westerra_app_more_tab
+                        startDestinationArgs =
+                            bundleOf(
+                                MoreJourney.INSTANCE_ID to
+                                        MoreMenuInstanceId.Custom(name = MENU_INSTANCE_ID_DEPOSIT_A_CHECK),
+                            )
+                    },
+                    // Disable cards management until requirement comes
+                   // DefaultUsCardsBottomMenuItem(),
+                    DefaultUsMoreBottomMenuItem(),
+                )
+            }
         }
     }
 }
